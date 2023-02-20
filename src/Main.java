1f95326f69e0f4b42633629bfd1da708b0e9e6df
Jeanette93Cph <jeanette.93@live.dk>
2023-02-20 10:43:50 +0100

    added switch case
    added switch case

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,28 @@ public class Main
 
     public static void main(String[] args)
     {
+        //System.out.println(getString("hvad hedder du ? "));
+        //System.out.println(getInt("angiv venligt din skostørrelse ?"));
+        //System.out.println(getInt("angiv venligt din højde ?"));
+
+        while(true)
+        {
+            switch(getString("angiv dit ønske ? "))
+            {
+                case"lån bog":
+                    System.out.println("du ønsker at låne en bog");
+                    break;
+                // case "stop":
+                // break;
+
+            }
+            if (getString("q for quit").equalsIgnoreCase("q"))
+            {
+                break;
+            }
+        }
+
+
 
     }
 
@@ -16,6 +38,26 @@ public class Main
         System.out.println(s + " : ");
         return scanner.nextLine();
 
+    }
+
+    public static int getInt(String s)
+    {
+        int res = 0;
+
+
+        while (true)
+        {
+            try
+            {
+                res = Integer.parseInt(getString(s));
+                return res;
+            }
+            catch(Exception e)
+            {
+                System.out.println("husk ikke tal ord");
+
+            }
+        }
     }
 
 
